69a1e36730f7cfd4748674525669a78ba6d407c7
unknown <daniellsilva@outlook.com>
2025-01-19 17:45:15 -0300

    Port scanning python

--- api/main.py
+++ api/main.py
@@ -1,1 +1,32 @@
-print("Olá mundo")
+import socket
+import sys
+
+# SOCK_STREAM = TCP
+# SOCK_DGRAM = UDP
+
+def port_scan_tcp(host, ports):
+    try:
+        host = socket.gethostbyname(host)
+        for port in ports:
+            # Scan de todas as portas
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((host, port))
+            print(f"Resultado da conexão com o {host}: {result}")
+
+            if result == 0:
+                print(f"Porta {port} está liberada")
+            else:
+                print(f"Porta {port} está bloqueada")
+            sock.close()
+    except InterruptedError:
+        print("Interrupted")
+        sys.exit()
+    except TimeoutError:
+        print("Timeout")
+        sys.exit()
+    except socket.error:
+        print("Server not responding!")
+        sys.exit()
+
+
